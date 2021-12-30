c64b771a803e897246d29d3cb240fdbdc2887eab
hext <hext123@foxmail.com>
2021-12-30 23:58:39 +0800

    添加 Clip Target

---
+++ PushDeerClip/PushDeerClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushDeerClipApp.swift
+//  PushDeerClip
+//
+//  Created by HEXT on 2021/12/30.
+//
+
+import SwiftUI
+
+@main
+struct PushDeerClipApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
