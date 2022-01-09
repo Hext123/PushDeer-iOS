0a5a5c280ae19749b9c442cd33fd7b6c76e9fb52
hext <hext123@foxmail.com>
2022-01-09 21:00:50 +0800

    消息列表支持markdown和image

--- PushDeer/View/MessageItemView.swift
+++ PushDeer/View/MessageItemView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import MarkdownUI
+//import NetworkImage
 
 struct MessageItemView: View {
   let messageItem: MessageItem
@@ -32,25 +34,60 @@ struct MessageItemView: View {
       }
       
       DeletableView(contentView: {
-        CardView {
-          HStack{
-            Text(messageItem.text)
-              .font(.system(size: 14))
-              .foregroundColor(Color(UIColor.darkGray))
-              .padding()
-            Spacer(minLength: 0)
-          }
-          .contextMenu {
-            Button("复制") {
-              UIPasteboard.general.string = "someText"
-            }
-          }
-        }
+        MessageContentView(messageItem: messageItem)
       }, deleteAction: deleteAction)
-        .padding(EdgeInsets(top: 10, leading: 26, bottom: 0, trailing: 24))
+        .padding(messageItem.type == "image" ? EdgeInsets.init() : EdgeInsets(top: 10, leading: 26, bottom: 0, trailing: 24))
       
     }
     .padding(.top, 25)
+  }
+}
+
+struct MessageContentView: View {
+  let messageItem: MessageItem
+  var body: some View {
+    switch messageItem.type {
+    case "markdown":
+      CardView {
+        Markdown(Document(messageItem.text))
+          .markdownStyle(
+            DefaultMarkdownStyle(
+              font: .system(size: 14),
+              foregroundColor: UIColor.darkGray
+            )
+          )
+          .padding()
+      }
+      
+    case "image":
+      //      NetworkImage.init(url: URL(string: messageItem.text))
+      //        .resizable().scaledToFill()
+      Markdown("![](\(messageItem.text))")
+        .markdownStyle(
+          DefaultMarkdownStyle(
+            font: .system(size: 14),
+            foregroundColor: UIColor.darkGray
+          )
+        )
+      //        .frame(width: .infinity)
+      
+      
+    default:
+      CardView {
+        HStack{
+          Text(messageItem.text)
+            .font(.system(size: 14))
+            .foregroundColor(Color(UIColor.darkGray))
+            .padding()
+          Spacer(minLength: 0)
+        }
+        .contextMenu {
+          Button("复制") {
+            UIPasteboard.general.string = messageItem.text
+          }
+        }
+      }
+    }
   }
 }
 
@@ -58,7 +95,17 @@ struct MessageItemView_Previews: PreviewProvider {
   static var previews: some View {
     VStack {
       MessageItemView(messageItem: MessageItem(id: 1, uid: "1", text: "纯文本的效果", desp: "", type: "text", created_at: "2022-01-08T18:00:48.000000Z")){}
+      MessageItemView(messageItem: MessageItem(id: 1, uid: "1", text: "纯文本的效果纯文本的效果纯文本的效果纯文本的效果纯文本的效果纯文本的效果纯文本的效果", desp: "", type: "text", created_at: "2022-01-08T18:00:48.000000Z")){}
+      MessageItemView(messageItem: MessageItem(id: 1, uid: "1", text: "https://blog.wskfz.com/usr/uploads/2018/06/2498727457.png", desp: "", type: "image", created_at: "2022-01-08T18:00:48.000000Z")){}
+      MessageItemView(messageItem: MessageItem(id: 1, uid: "1", text: "https://blog.wskfz.com/usr/uploads/2018/06/2151130181.png", desp: "", type: "image", created_at: "2022-01-08T18:00:48.000000Z")){}
+      MessageItemView(messageItem: MessageItem(id: 1, uid: "1", text: "https://blog.wskfz.com/usr/uploads/2018/06/1718629805.png", desp: "", type: "image", created_at: "2022-01-08T18:00:48.000000Z")){}
       MessageItemView(messageItem: MessageItem(id: 1, uid: "1", text: "*MarkDown*的**效果**", desp: "", type: "markdown", created_at: "2021-12-28T13:44:48.000000Z")){}
+      MessageItemView(messageItem: MessageItem(id: 1, uid: "1", text: """
+It's very easy to make some words **bold** and other words *italic* with Markdown.
+
+**Want to experiment with Markdown?** Play with the [reference CommonMark
+implementation](https://spec.commonmark.org/dingus/).
+""", desp: "", type: "markdown", created_at: "2021-12-28T13:44:48.000000Z")){}
       Spacer()
     }
   }
