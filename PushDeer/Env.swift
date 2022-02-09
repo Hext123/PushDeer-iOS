7badf901352182e24f0a8fdc39354f46c7fe93c0
hext <hext123@foxmail.com>
2022-02-09 23:10:54 +0800

    完善SelfHosted配置, 可以直接一套代码跑出两种版本

--- PushDeer/Env.swift
+++ PushDeer/Env.swift
@@ -10,7 +10,13 @@ import Foundation
 /// APP运行环境
 struct Env {
   /// 是否是自建服务, 自建服务为 true, 在线服务为 false
-  static let isSelfHosted = true
+  static let isSelfHosted: Bool = {
+    #if SELFHOSTED
+    return true
+    #else
+    return false
+    #endif
+  }()
   /// 在线版本的 Api Endpoint
   static let onlineApiEndpoint = "http://pushdeer.wskfz.com:8800"
   /// AppStore 的 appId, 自建版: 1608017631; 在线版: 1596771139
